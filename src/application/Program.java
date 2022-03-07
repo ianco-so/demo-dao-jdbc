340920f3e2efac9e1a98b38528857e32dd1ef174
Ianco Soares <ianco.so@gmail.com>
2022-03-07 00:26:09 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,8 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== Test 1: Seller findById() ===");
@@ -26,14 +29,23 @@ public class Program {
 		for (Seller obj : list)	System.out.println(obj);
 		
 		System.out.println("\n=== Test 4: Seller insert() ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		Seller newSeller = new Seller(8, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("inserted: New Id = "+newSeller.getId());
 		
+		/*
 		System.out.println("\n=== Test 5: Seller update() ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Marta Wayne");
 		sellerDao.update(seller);
 		System.out.println("update completed!");
+		*/
+		System.out.println("\n=== Test 6: Seller delete() ===");
+		System.out.println("Enter id for delete test:");
+		int id = sc.nextInt();
+		sellerDao.deletById(id);
+		System.out.println("delete completed");
+		
+		sc.close();
 	}
 }
