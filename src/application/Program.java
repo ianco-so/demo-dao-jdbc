68ed04f81850cc5c83327876bfeea5ee7ef5e422
Ianco Soares <ianco.so@gmail.com>
2022-03-07 00:12:42 -0300

    update implemantation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,5 +29,11 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("inserted: New Id = "+newSeller.getId());
+		
+		System.out.println("\n=== Test 5: Seller update() ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		sellerDao.update(seller);
+		System.out.println("update completed!");
 	}
 }
